46914f541de68975a0a6396128b8f217052ff764
haitwang <haitwang@ebay.com>
2021-03-17 22:11:56 +0800

    add 两个数组的交集

--- personal-code/数组/两个数组的交集/intersect.go
+++ personal-code/数组/两个数组的交集/intersect.go
@@ -1,27 +1,47 @@
 package main
-import "fmt"
+
+import (
+	"fmt"
+	"sort"
+)
 
 func intersect(nums1 []int, nums2 []int) []int {
-	if len(nums1) > len(nums2) {
-		return intersect(nums2, nums1)
+	arrDict := map[int]int{}
+	for _, value := range nums1 {
+		arrDict[value]++
 	}
-	m := map[int]int{}
-	for _, num := range nums1 {
-		m[num]++
-	}
-
-	intersection := []int{}
-	for _, num := range nums2 {
-		if m[num] > 0 {
-			intersection = append(intersection, num)
-			m[num]--
+	index := 0
+	for _, value := range nums2 {
+		if arrDict[value] > 0 {
+			arrDict[value]--
+			nums2[index] = value
+			index++
 		}
 	}
-	return intersection
+	return nums2[0:index]
+}
+func intersectForSortedArray(nums1 []int, nums2 []int) []int {
+	sort.Ints(nums1)
+	sort.Ints(nums2)
+	fmt.Println(nums1, nums2)
+	i, j, index := 0, 0, 0
+	for i < len(nums1) && j < len(nums2) {
+		if nums1[i] > nums2[j] {
+			j++
+		} else if nums1[i] < nums2[j] {
+			i++
+		} else {
+			nums1[index] = nums1[i]
+			index++
+			i++
+			j++
+		}
+	}
+	return nums1[0:index]
 }
 func main() {
 	nums1 := []int{4, 9, 5, 4, 4}
 	nums2 := []int{9, 4, 9, 8, 4, 6}
-	fmt.Println(nums1, nums2)
 	fmt.Println(intersect(nums1, nums2))
+	fmt.Println(intersectForSortedArray(nums1, nums2))
 }
