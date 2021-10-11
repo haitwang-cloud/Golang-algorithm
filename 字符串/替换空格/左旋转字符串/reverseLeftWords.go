d0f6cd3181190a8c72b766e0e0161cf530fc0672
haitwang <haitwang@ebay.com>
2021-10-11 21:47:05 +0800

    add new reverseLeftWordsNew

--- 字符串/替换空格/左旋转字符串/reverseLeftWords.go
+++ 字符串/替换空格/左旋转字符串/reverseLeftWords.go
@@ -7,8 +7,28 @@ func reverseLeftWords(s string, n int) string {
 	return result_right + result_left
 
 }
+func reverseLeftWordsNew(s string, n int) string {
+	str_result := []byte(s)
+	// 1. 反转前n个字符
+	// 2. 反转第n到end字符
+	// 3. 反转整个字符
+	reverse(str_result, 0, n-1)
+	reverse(str_result, n, len(str_result)-1)
+	reverse(str_result, 0, len(str_result)-1)
+	return string(str_result)
+
+}
+func reverse(b []byte, left, right int) {
+	for left < right {
+		b[left], b[right] = b[right], b[left]
+		left++
+		right--
+	}
+
+}
 
 func main() {
 	test := "abcdefg"
 	fmt.Println(reverseLeftWords(test, 2))
+	fmt.Println(reverseLeftWordsNew(test, 2))
 }
