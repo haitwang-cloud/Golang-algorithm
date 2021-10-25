f890d5fc849a36a09df76b82588de928394ff3c8
haitwang <haitwang@ebay.com>
2021-10-25 21:10:53 +0800

    add day-08-code

--- Day-08-动态规划/股票的最大利润/maxProfit.go
+++ Day-08-动态规划/股票的最大利润/maxProfit.go
@@ -1,12 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+// 如果我们真的在买卖股票，我们肯定会想：
+// 如果我是在历史最低点买的股票就好了！太好了，
+// 在题目中，我们只要用一个变量记录一个历史最低价格 minprice，
+// 我们就可以假设自己的股票是在那天买的。那么我们在第 i 天卖出股票能得到的利润就是 prices[i] - minprice
 
 func maxProfit(prices []int) int {
-	maxValue := 0
-	for i := 1; i < len(prices); i++ {
-		if prices[i] > prices[i-1] {
-			maxValue += prices[i] - prices[i-1]
+	maxValue, minPrice := 0, math.MaxInt64
+	for i := 0; i < len(prices); i++ {
+		if prices[i] < minPrice {
+			minPrice = prices[i]
+		} else if (prices[i] - minPrice) > maxValue {
+			maxValue = prices[i] - minPrice
 		}
 
 	}
