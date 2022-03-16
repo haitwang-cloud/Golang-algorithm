b430f71356a36f605597597d10ddc1ef2d2f146b
haitwang <haitao_wht@outlook.com>
2022-03-16 21:51:48 +0800

    add 前 K 个高频元素

--- routine-training/排序/前 K 个高频元素/topKFrequent.go
+++ routine-training/排序/前 K 个高频元素/topKFrequent.go
@@ -11,16 +11,16 @@ func topKFrequent(nums []int, k int) []int {
 	for _, num := range nums {
 		occurrences[num]++
 	}
-	values := [][]int{}
+	var values [][]int
 	for key, value := range occurrences {
 		values = append(values, []int{key, value})
 	}
 	ret := make([]int, k)
-	qsort(values, 0, len(values)-1, ret, 0, k)
+	quickSort(values, 0, len(values)-1, ret, 0, k)
 	return ret
 }
 
-func qsort(values [][]int, start, end int, ret []int, retIndex, k int) {
+func quickSort(values [][]int, start, end int, ret []int, retIndex, k int) {
 	rand.Seed(time.Now().UnixNano())
 	picked := rand.Int()%(end-start+1) + start
 	values[picked], values[start] = values[start], values[picked]
@@ -36,14 +36,14 @@ func qsort(values [][]int, start, end int, ret []int, retIndex, k int) {
 	}
 	values[start], values[index] = values[index], values[start]
 	if k <= index-start {
-		qsort(values, start, index-1, ret, retIndex, k)
+		quickSort(values, start, index-1, ret, retIndex, k)
 	} else {
 		for i := start; i <= index; i++ {
 			ret[retIndex] = values[i][0]
 			retIndex++
 		}
 		if k > index-start+1 {
-			qsort(values, index+1, end, ret, retIndex, k-(index-start+1))
+			quickSort(values, index+1, end, ret, retIndex, k-(index-start+1))
 		}
 	}
 }
