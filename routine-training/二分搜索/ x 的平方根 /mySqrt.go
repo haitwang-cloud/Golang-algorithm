7a644a70bb9050f7ce8d08d75d78ccdd1885869c
haitwang <haitao_wht@outlook.com>
2022-05-17 22:10:16 +0800

    add x 的平方根

---
+++ routine-training/二分搜索/ x 的平方根 /mySqrt.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func mySqrt(x int) int {
+	if x < 2 {
+		return x
+	}
+	left, right := 1, x
+	for left <= right {
+		mid := left + (right-left)/2
+		if mid*mid == x {
+			return mid
+		} else if mid*mid < x {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return right
+}
+
+func main() {
+	test := 8
+	fmt.Println(mySqrt(test))
+}
