34128e8c60f071a9806960bb2b224d1d99210aa2
haitwang <haitao_wht@outlook.com>
2022-05-22 17:43:05 +0800

    add滑动窗口

---
+++ routine-training/滑动窗口/minSubArrayLen.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func minSubArrayLen(target int, nums []int) int {
+	if len(nums) < 1 {
+		return 0
+	}
+	start, end, sum, result := 0, 0, 0, math.MaxInt32
+	for end < len(nums) {
+		sum += nums[end]
+		for sum >= target {
+			subLength := end - start + 1
+			if result > subLength {
+				result = subLength
+			}
+			sum -= nums[start]
+			start++
+		}
+		end++
+	}
+	if result == math.MaxInt32 {
+		return 0
+	}
+	return result
+
+}
+
+func main() {
+	nums, test := []int{2, 3, 1, 2, 4, 3}, 7
+	fmt.Println(minSubArrayLen(test, nums))
+}
