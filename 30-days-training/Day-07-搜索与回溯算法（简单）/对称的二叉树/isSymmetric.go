f0a91711644381c68257a10a55eb0b737177f328
haitwang <haitao_wht@outlook.com>
2022-09-10 22:01:38 +0800

    add day07 code

--- 30-days-training/Day-07-搜索与回溯算法（简单）/对称的二叉树/isSymmetric.go
+++ 30-days-training/Day-07-搜索与回溯算法（简单）/对称的二叉树/isSymmetric.go
@@ -10,21 +10,20 @@ func isSymmetric(root *TreeNode) bool {
 	if root == nil {
 		return true
 	}
-	return recur(root.Left, root.Right)
+	return validate(root.Left, root.Right)
 }
 
-func recur(L, R *TreeNode) bool {
+func validate(L, R *TreeNode) bool {
+
+	/*
+		当节点A和节点B都为空时，匹配成功，返回TRUE
+		当节点A为空，或节点B为空，或节点A和节点B的值不相同，匹配失败，返回FALSE
+	*/
 	if L == nil && R == nil {
 		return true
 	}
 	if L == nil || R == nil || L.Val != R.Val {
 		return false
 	}
-	return recur(L.Left, R.Right) && recur(L.Right, R.Left)
+	return validate(L.Left, R.Right) && validate(L.Right, R.Left)
 }
-
-// boolean recur(TreeNode L, TreeNode R) {
-// 	if(L == null && R == null) return true;
-// 	if(L == null || R == null || L.val != R.val) return false;
-// 	return recur(L.left, R.right) && recur(L.right, R.left);
-// }
--- 30-days-training/Day-07-搜索与回溯算法（简单）/树的子结构/isSubStructure.go
+++ 30-days-training/Day-07-搜索与回溯算法（简单）/树的子结构/isSubStructure.go
@@ -7,6 +7,11 @@ type TreeNode struct {
 }
 
 func isSubStructure(A *TreeNode, B *TreeNode) bool {
+
+	/*
+		当节点A和节点B都为空时，匹配成功，返回TRUE
+		当节点A或者节点B有一个为空是，说明已经匹配失败,返回FALSE
+	*/
 	if A == nil && B == nil {
 		return true
 	}
@@ -14,39 +19,24 @@ func isSubStructure(A *TreeNode, B *TreeNode) bool {
 		return false
 	}
 
-	var ret bool
-
-	//当在 A 中找到 B 的根节点时，进入helper递归校验
-	if A.Val == B.Val {
-		ret = helper(A, B)
-	}
-
-	//ret == false，说明 B 的根节点不在当前 A 树顶中，进入 A 的左子树进行递归查找
-	if !ret {
-		ret = isSubStructure(A.Left, B)
-	}
-
-	//当 B 的根节点不在当前 A 树顶和左子树中，进入 A 的右子树进行递归查找
-	if !ret {
-		ret = isSubStructure(A.Right, B)
-	}
-	return ret
-
-	//利用 || 的短路特性可写成
-	//return helper(A,B) || isSubStructure(A.Left,B) || isSubStructure(A.Right,B)
+	return validate(A, B) || isSubStructure(A.Left, B) || isSubStructure(A.Right, B)
 }
 
-//helper 校验 B 是否与 A 的一个子树拥有相同的结构和节点值
-func helper(a, b *TreeNode) bool {
-	if b == nil {
+//validate 校验 B 是否与 A 的一个子树拥有相同的结构和节点值
+func validate(A, B *TreeNode) bool {
+
+	/*
+		当节点B为空，说明B已经匹配完成,返回TRUE
+		当节点A为空, 说明已经越过 A 叶子节点，匹配失败，返回FALSE
+		当节点A和节点B的值不相同时，匹配失败，返回FALSE
+	*/
+	if B == nil {
 		return true
 	}
-	if a == nil {
-		return false
-	}
-	if a.Val != b.Val {
+	if A == nil || A.Val != B.Val {
 		return false
 	}
 	//a.Val == b.Val 递归校验 A B 左子树和右子树的结构和节点是否相同
-	return helper(a.Left, b.Left) && helper(a.Right, b.Right)
+	return validate(A.Left, B.Left) && validate(A.Right, B.Right)
+
 }
