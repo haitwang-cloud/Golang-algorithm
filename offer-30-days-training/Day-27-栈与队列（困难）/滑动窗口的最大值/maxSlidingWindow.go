917fbe7cee7b7a96cce013545c0f7869dc6c8560
Haitwang <haitwang@ebay.com>
2022-09-30 21:48:33 +0800

    add day-27 code

---
+++ offer-30-days-training/Day-27-栈与队列（困难）/滑动窗口的最大值/maxSlidingWindow.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+//https://leetcode.cn/problems/hua-dong-chuang-kou-de-zui-da-zhi-lcof/solution/mian-shi-ti-59-i-hua-dong-chuang-kou-de-zui-da-1-6/
+//	链接：https://leetcode.cn/problems/hua-dong-chuang-kou-de-zui-da-zhi-lcof/solution/hua-dong-chuang-kou-de-zui-da-zhi-by-lee-ymyo/
+func maxSlidingWindow(nums []int, k int) []int {
+
+	q := []int{}
+	push := func(i int) {
+		for len(q) > 0 && nums[i] >= nums[q[len(q)-1]] {
+			q = q[:len(q)-1]
+		}
+		q = append(q, i)
+	}
+
+	for i := 0; i < k; i++ {
+		push(i)
+	}
+
+	n := len(nums)
+	ans := make([]int, 1, n-k+1)
+	ans[0] = nums[q[0]]
+	for i := k; i < n; i++ {
+		push(i)
+		for q[0] <= i-k {
+			q = q[1:]
+		}
+		ans = append(ans, nums[q[0]])
+	}
+	return ans
+}
+func main() {
+	nums, k := []int{1, 3, -1, -3, 5, 3, 6, 7}, 3
+	fmt.Println(maxSlidingWindow(nums, k))
+}
