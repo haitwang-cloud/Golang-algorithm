24ba7a51a5cb20b191730bb62bca730a967635ef
haitwang-cloud <haitao_wht@outlook.com>
2023-01-02 20:42:04 +0800

    :memo: add new solution for frequencySort

--- codes/routine-training/排序/根据字符出现频率排序/frequencySort.go
+++ codes/routine-training/排序/根据字符出现频率排序/frequencySort.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"sort"
 )
-
+// https://leetcode.cn/problems/sort-characters-by-frequency/solution/gen-ju-zi-fu-chu-xian-pin-lu-pai-xu-by-l-zmvy/
 func frequencySort(s string) string {
 	cnt := map[byte]int{}
 	for i := range s {
@@ -29,7 +29,38 @@ func frequencySort(s string) string {
 	return string(ans)
 }
 
+func frequencySortNew(s string) string {
+    cnt := map[byte]int{}
+    maxFreq := 0
+    for i := range s {
+        cnt[s[i]]++
+        maxFreq = max(maxFreq, cnt[s[i]])
+    }
+
+    buckets := make([][]byte, maxFreq+1)
+    for ch, c := range cnt {
+        buckets[c] = append(buckets[c], ch)
+    }
+
+    ans := make([]byte, 0, len(s))
+    for i := maxFreq; i > 0; i-- {
+        for _, ch := range buckets[i] {
+            ans = append(ans, bytes.Repeat([]byte{ch}, i)...)
+        }
+    }
+    return string(ans)
+}
+
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
+
+
 func main() {
 	test := "cccbaaa"
 	fmt.Println(frequencySort(test))
+	fmt.Println(frequencySortNew(test))
 }
