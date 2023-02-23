a20c23b492dc5a8430787920d35289ffe2da98ba
haitwang-cloud <haitao_wht@outlook.com>
2023-02-23 22:08:07 +0800

    add removeNthFromEnd

---
+++ codes/routine-training/链表/删除链表的倒数第 N 个结点/removeNthFromEnd.go
@@ -0,0 +1,29 @@
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+
+	dummyHead := &ListNode{}
+	dummyHead.Next = head
+	fast, slow := dummyHead, dummyHead
+	for i := 0; i < n; i++ {
+		if fast.Next != nil {
+			fast = fast.Next
+		}
+	}
+	for fast.Next != nil {
+		fast = fast.Next
+		slow = slow.Next
+	}
+	slow.Next = slow.Next.Next
+	return dummyHead.Next
+}
+
+func main() {
+	testList := &ListNode{1, &ListNode{2, &ListNode{3, &ListNode{4, &ListNode{5, nil}}}}} // 1->2->3->4->5
+	removeNthFromEnd(testList, 2)
+}
