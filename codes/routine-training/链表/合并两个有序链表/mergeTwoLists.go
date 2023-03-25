7c08165c3c3e70742a1968be231b04f5877e3b8f
haitwang-cloud <haitao_wht@outlook.com>
2023-03-25 17:53:25 +0800

    :memo: add mergeTwoLists

---
+++ codes/routine-training/链表/合并两个有序链表/mergeTwoLists.go
@@ -0,0 +1,32 @@
+package main
+
+// Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	dummyHead := &ListNode{-1, nil}
+	p, p1, p2 := dummyHead, list1, list2
+	for p1 != nil && p2 != nil {
+		if p1.Val < p2.Val {
+			p.Next = p1
+			p1 = p1.Next
+		} else {
+			p.Next = p2
+			p2 = p2.Next
+		}
+		p = p.Next
+	}
+	if p1 != nil {
+		p.Next = p1
+	}
+
+	if p2 != nil {
+		p.Next = p2
+	}
+
+	return dummyHead.Next
+
+}
