7cf324a65631ae390f26a986bc94b8b26a4c9e43
haitwang-cloud <haitao_wht@outlook.com>
2023-03-26 16:59:11 +0800

    :memo: add环形链表

---
+++ codes/routine-training/链表/环形链表/hasCycle_test.go
@@ -0,0 +1,29 @@
+package main
+
+import "testing"
+
+func Test_hasCycle(t *testing.T) {
+	type args struct {
+		head *ListNode
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "test1",
+			args: args{
+				head: &ListNode{1, &ListNode{2, &ListNode{3, &ListNode{4, &ListNode{5, nil}}}}},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := hasCycle(tt.args.head); got != tt.want {
+				t.Errorf("hasCycle() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
