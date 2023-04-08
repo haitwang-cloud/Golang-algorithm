1031e305fa44b36636a73c08b2c4960a4d424a50
haitwang-cloud <haitao_wht@outlook.com>
2023-04-08 12:04:31 +0800

    add 删除有序数组中的重复项

---
+++ codes/routine-training/数组/删除有序数组中的重复项/removeDuplicates.go
@@ -0,0 +1,16 @@
+package main
+
+func removeDuplicates(nums []int) int {
+	if len(nums) < 1 {
+		return 0
+	}
+	slow, fast := 0, 0
+	for fast < len(nums) {
+		if nums[slow] != nums[fast] {
+			slow++
+			nums[slow] = nums[fast]
+		}
+		fast++
+	}
+	return slow + 1
+}
