fb6bafa4943fc21b7f5b17b1dcaa7cf04f07038b
haitwang-cloud <haitao_wht@outlook.com>
2023-04-20 10:09:15 +0800

    update reverseList & add maxDepth

--- codes/routine-training/链表/反转链表/reverseList.go
+++ codes/routine-training/链表/反转链表/reverseList.go
@@ -1,35 +1,22 @@
 package main
 
+// https://leetcode.cn/problems/fan-zhuan-lian-biao-lcof/
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
 }
 
 func reverseList(head *ListNode) *ListNode {
+
 	cur := head
-	var pre *ListNode
+	var dummyHead *ListNode
 	for cur != nil {
 		next := cur.Next
-		cur.Next = pre
-		pre = cur
+		cur.Next = dummyHead
+		dummyHead = cur
 		cur = next
 	}
-	return pre
+	return dummyHead
 
 }
-
-// func reverse(pre, cur *ListNode) *ListNode {
-// 	if cur == nil {
-// 		return pre
-// 	}
-// 	next := cur.Next
-// 	cur.Next = pre
-// 	return reverse(cur, next)
-// }
-
-// func reverseList2(head *ListNode) *ListNode {
-// 	if head == nil {
-// 		return nil
-// 	}
-// 	return reverse(nil, head)
-// }
