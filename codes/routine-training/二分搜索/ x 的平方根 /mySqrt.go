7874e989fac7789799d079c7d561dc94063a439c
haitwang-cloud <haitao_wht@outlook.com>
2023-04-24 08:51:23 +0800

    add UT for sqrt

--- codes/routine-training/二分搜索/ x 的平方根 /mySqrt.go
+++ codes/routine-training/二分搜索/ x 的平方根 /mySqrt.go
@@ -1,16 +1,12 @@
 package main
 
-import "fmt"
-
 // https://leetcode.cn/problems/sqrtx/solution/x-de-ping-fang-gen-by-leetcode-solution/
 
 /*
-
 由于x 平方根的整数部分ans是满足k^2≤x的最大k值，因此我们可以对k进行二分查找，从而得到答案。
 二分查找的下界为0，上界可以粗略地设定为x。在
 二分查找的每一步中，我们只需要比较中间元素mid的平方与x的大小关系，并通过比较的结果调整上下界的范围
 。由于我们所有的运算都是整数运算，不会存在误差，因此在得到最终的答案ans后，也就不需要再去尝试ans+1 了。
-
 */
 func mySqrt(x int) int {
 	if x < 2 {
@@ -29,9 +25,4 @@ func mySqrt(x int) int {
 
 	}
 	return right
-}
-
-func main() {
-	test := 9
-	fmt.Println(mySqrt(test))
 }
---
+++ codes/routine-training/二分搜索/ x 的平方根 /mySqrt_test.go
@@ -0,0 +1,36 @@
+package main
+
+import "testing"
+
+func Test_mySqrt(t *testing.T) {
+	type args struct {
+		x int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test1",
+			args: args{
+				x: 9,
+			},
+			want: 3,
+		},
+		{
+			name: "test2",
+			args: args{
+				x: 17,
+			},
+			want: 4,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := mySqrt(tt.args.x); got != tt.want {
+				t.Errorf("mySqrt() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
