6671e89a7bc6d4521bf19e34d76d8b5ed4fa5d1a
haitwang-cloud <haitao_wht@outlook.com>
2023-07-09 16:53:04 +0800

    update preorder

--- codes/routine-training/ 二叉树/二叉树的前序遍历/preorderTraversal.go
+++ codes/routine-training/ 二叉树/二叉树的前序遍历/preorderTraversal.go
@@ -12,29 +12,10 @@ func preorderTraversal(root *TreeNode) []int {
 	if root == nil {
 		return res
 	}
-	// 前序遍历：根左右
+	// 前序遍历
 	res = append(res, root.Val)
-	// 递归遍历左子树
 	res = append(res, preorderTraversal(root.Left)...)
-	// 递归遍历右子树
 	res = append(res, preorderTraversal(root.Right)...)
-	return res
-}
 
-func preorderTraverse(root *TreeNode) []int {
-	//递归实现
-	var res []int
-	preorder(root, &res)
 	return res
-}
-
-func preorder(node *TreeNode, res *[]int) {
-	if node == nil { //递归停止条件
-		return
-	} //前序遍历：中左右
-	*res = append(*res, node.Val)
-	// 递归遍历左子树
-	preorder(node.Left, res)
-	// 递归遍历右子树
-	preorder(node.Right, res)
 }
--- codes/routine-training/ 二叉树/二叉树的最大深度/maxDepth.go
+++ codes/routine-training/ 二叉树/二叉树的最大深度/maxDepth.go
@@ -11,12 +11,17 @@ func maxDepth(root *TreeNode) int {
 	if root == nil {
 		return 0
 	}
-	return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
-
+	leftMax := maxDepth(root.Left)
+	rightMax := maxDepth(root.Right)
+	res := max(leftMax, rightMax) + 1
+	return res
 }
+
 func max(a, b int) int {
+
 	if a > b {
 		return a
 	}
 	return b
+
 }
